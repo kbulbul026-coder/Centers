82ea925b7dd191e96d279ec77135898abfc260e3
kbulbul026-coder <kbulbul026@gmail.com>
2025-12-17 14:51:06 +0530

    add app and centers

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # This will look for templates/centers.html
+    return render_template('centers.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
