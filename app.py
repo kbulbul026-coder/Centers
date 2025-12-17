a65bb08c28314a6d6c36c7b61079c60eafaa257b
kbulbul026-coder <kbulbul026@gmail.com>
2025-12-17 17:12:39 +0530

    addindex,mission

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# होम पेज - यहाँ से आप दोनों टेबल पर जा सकते हैं
 @app.route('/')
 def index():
-    # This will look for templates/centers.html
+    return render_template('index.html')
+
+# पुराना Centers of Life पेज
+@app.route('/centers')
+def centers():
     return render_template('centers.html')
+
+# आपका नया मिशन वक्तव्य पेज
+@app.route('/mission')
+def mission():
+    return render_template('mission.html')
 
 if __name__ == '__main__':
     app.run(debug=False)
