ebc925deb0b911088d4c1209627d25dd1ff1f1a6
kbulbul026-coder <kbulbul026@gmail.com>
2025-12-19 09:48:55 +0530

    update index page

--- app.py
+++ app.py
@@ -4,26 +4,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('centers_matrix.html') # अब सीधे पहली टेबल खुलेगी
+    return render_template('index.html')
 
-@app.route('/perspective')
-def perspective():
+@app.route('/centers-matrix')
+def centers_matrix():
+    return render_template('centers_matrix.html')
+
+@app.route('/perspective-matrix')
+def perspective_matrix():
     return render_template('perspective_matrix.html')
 
-@app.route('/skills')
-def skills():
+@app.route('/skills-growth')
+def skills_growth():
     return render_template('skills_growth.html')
-
-@app.route('/mission')
-def mission():
-    return render_template('mission.html')
-
 
 @app.route('/stoplight')
 def stoplight():
     return render_template('stoplight.html')
 
-
+@app.route('/mission')
+def mission():
+    return render_template('mission.html')
 
 if __name__ == '__main__':
     app.run(debug=False)
