123291cfe1647659e1e61df341450a7550366df8
kbulbul026-coder <kbulbul026@gmail.com>
2025-12-19 06:50:30 +0530

    add EI

--- app.py
+++ app.py
@@ -4,19 +4,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('centers_matrix.html') # अब सीधे पहली टेबल खुलेगी
 
-@app.route('/centers-matrix')
-def centers_matrix():
-    return render_template('centers_matrix.html')
-
-@app.route('/perspective-matrix')
-def perspective_matrix():
+@app.route('/perspective')
+def perspective():
     return render_template('perspective_matrix.html')
+
+@app.route('/skills')
+def skills():
+    return render_template('skills_growth.html')
 
 @app.route('/mission')
 def mission():
     return render_template('mission.html')
+
+
+@app.route('/stoplight')
+def stoplight():
+    return render_template('stoplight.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=False)
