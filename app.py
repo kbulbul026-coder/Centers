d504e65609231667c8e1b8c30786f4e7b514bd74
kbulbul026-coder <kbulbul026@gmail.com>
2025-12-22 12:37:23 +0530

    add father_forgets.html

--- app.py
+++ app.py
@@ -3,53 +3,43 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def index(): return render_template('index.html')
 
 @app.route('/centers-matrix')
-def centers_matrix():
-    return render_template('centers_matrix.html')
+def centers(): return render_template('centers_matrix.html')
 
 @app.route('/perspective-matrix')
-def perspective_matrix():
-    return render_template('perspective_matrix.html')
+def perspective(): return render_template('perspective_matrix.html')
 
 @app.route('/skills-growth')
-def skills_growth():
-    return render_template('skills_growth.html')
+def skills(): return render_template('skills_growth.html')
 
 @app.route('/stoplight')
-def stoplight():
-    return render_template('stoplight.html')
+def stoplight(): return render_template('stoplight.html')
 
 @app.route('/victory-hour')
-def victory_hour():
-    return render_template('victory_hour.html')
+def victory(): return render_template('victory_hour.html')
 
 @app.route('/success-denominator')
-def success_denominator():
-    return render_template('success_denominator.html')
+def success(): return render_template('success_denominator.html')
 
 @app.route('/legacy-service')
-def legacy_service():
-    return render_template('legacy_service.html')
-
-@app.route('/mission')
-def mission():
-    return render_template('mission.html')
+def legacy(): return render_template('legacy_service.html')
 
 @app.route('/win-win')
-def win_win():
-    return render_template('win_win.html')
+def winwin(): return render_template('win_win.html')
 
 @app.route('/maturity-continuum')
-def maturity():
-    return render_template('maturity_continuum.html')
+def maturity(): return render_template('maturity_continuum.html')
 
 @app.route('/ppc-balance')
-def ppc_balance():
-    return render_template('ppc_balance.html')
+def ppc(): return render_template('ppc_balance.html')
 
+@app.route('/father-forgets')
+def father(): return render_template('father_forgets.html')
+
+@app.route('/mission')
+def mission(): return render_template('mission.html')
 
 if __name__ == '__main__':
     app.run(debug=False)
